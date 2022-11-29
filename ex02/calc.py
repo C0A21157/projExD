c00dd218ceb4d33dde3a1b608ecc4e565e0827b2
matsuo <c0a2115799@edu.teu.ac.jp>
2022-11-29 14:38:09 +0900

    練習2-5

--- ex02/calc.py
+++ ex02/calc.py
@@ -12,10 +12,27 @@ root=tk.Tk() #Pythonで大文字から始まるものはクラス名これをroo
 
 root.geometry("300x500")
 
-r=0 
+entry=tk.Entry(root,justify="right",width=10,font=("",40))#代替半角３０文字くらいはいるよって意味
+
+entry.grid(row=0,column=0,columnspan=3)
+
+
+r=1
 c=0
+
+
 for num in range(9,-1,-1):
     button=tk.Button(root,text=f"{num}",width=4,height=2,font=("",30))
+    button.grid(row=r,column=c)
+    button.bind("<1>",button_click)
+    c+=1
+    if c%3==0:
+        r+=1
+        c=0
+
+operators=["+","="]
+for ope in operators:
+    button=tk.Button(root,text=f"{ope}",width=4,height=2,font=("",30))
     button.grid(row=r,column=c)
     button.bind("<1>",button_click)
     c+=1
