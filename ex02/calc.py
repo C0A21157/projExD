7a47307e800173f471cc3f95ee884bc88b148e86
matsuo <c0a2115799@edu.teu.ac.jp>
2022-11-29 14:23:52 +0900

    練習2-3

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,11 +1,27 @@
 import tkinter as tk
-#import tkinter.messagebox as tkm
+import tkinter.messagebox as tkm
+
+def button_click(event):
+    btn=event.widget
+    num=btn["text"]
+    tkm.showinfo("",f"{num}ボタンがクリックされました")
 
 
 
 root=tk.Tk() #Pythonで大文字から始まるものはクラス名これをrootという変数に代入
 
 root.geometry("300x500")
+
+r=0 
+c=0
+for num in range(9,-1,-1):
+    button=tk.Button(root,text=f"{num}",width=4,height=2,font=("",30))
+    button.grid(row=r,column=c)
+    button.bind("<1>",button_click)
+    c+=1
+    if c%3==0:
+        r+=1
+        c=0
 
 
 root.mainloop() #ウィンドウ表示
