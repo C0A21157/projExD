465c7fcc62aaac0c3f2c0fee172996b50339167d
matsuo <c0a2115799@edu.teu.ac.jp>
2022-12-13 15:31:58 +0900

    4-8改訂

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -3,16 +3,16 @@ import random
 import sys
 
 
-def chec_bound(obj_rct,scr_rct):
-    #第一引数：こうかとんrectまたは爆弾ect
-    #範囲内:　＋１/範囲外： -1
-    yoko,tate =+1,+1
-    if obj_rct.left < scr_rct.left or sct_rct.right < obg_rct.right:
-        hoge
-    if obj_rct.top < scr_rct.top or sct_rct.bottom < obg_rct.bottom:
-        fuge
-    return yoko,tate
-    
+def check_bound(obj_rct, scr_rct):
+    # 第1引数：こうかとんrectまたは爆弾rect
+    # 第2引数：スクリーンrect
+    # 範囲内：+1／範囲外：-1
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko, tate
 
 
 def main():
@@ -20,7 +20,7 @@ def main():
     # 練習１
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
-    scrn_rct=scrn.sfc.get_rect()
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
@@ -32,20 +32,15 @@ def main():
     # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
     scrn_sfc.blit(tori_sfc, tori_rct) 
 
-    pg.display.update()
-    clock.tick(1000)
-
-    bomb_sfc=pg.Surface((20,20)) #正方形の空のSurface
-    bomb_sfc.set__colorkey((0,0,0))
-    pg.draw.circle(bomb_sfc,(255,0,0),(10,10),10)
-    bomb_rct=bomb_sfc.get_rect()
-    bomb_rct.centerx=random.randint(0,scrn_rct.width)
-    bomb_rct.centery=random.randint(scrn_rct.height,0)
-    scrn_sfc.blit(bomb_sfc,bomb_rct)
-
-    bomb_sfc.blit(bomb_sfc,bomb_rct)
-    
-
+    # 練習５
+    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
+    bomb_sfc.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct) 
+    vx, vy = +1, +1
 
     # 練習２
     while True:
@@ -55,26 +50,38 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-        key_dct=pg.key.get_pressed()#辞書型
+        # 練習4
+        key_dct = pg.key.get_pressed() # 辞書型
         if key_dct[pg.K_UP]:
-            tori_rct.ventery -= 1
-
+            tori_rct.centery -= 1
         if key_dct[pg.K_DOWN]:
             tori_rct.centery += 1
-        
         if key_dct[pg.K_LEFT]:
-            tori_rct.centerx -=1
-
+            tori_rct.centerx -= 1
         if key_dct[pg.K_RIGHT]:
-            tori_rct.centerx +=1
+            tori_rct.centerx += 1
+        if check_bound(tori_rct, scrn_rct) != (+1, +1):
+            # どこかしらはみ出ていたら
+            if key_dct[pg.K_UP]:
+                tori_rct.centery += 1
+            if key_dct[pg.K_DOWN]:
+                tori_rct.centery -= 1
+            if key_dct[pg.K_LEFT]:
+                tori_rct.centerx += 1
+            if key_dct[pg.K_RIGHT]:
+                tori_rct.centerx -= 1            
+        scrn_sfc.blit(tori_sfc, tori_rct) 
 
-        vx,vy= +1,+1
-        bomb_rct.move_ip(vx,vy)
-        yoko,tate=check_bound(bomb_rct,scrn_rct)
-        vx *=yoko
-        vy *=tate
-        bomb_rct.move_ip(vx,vy)
-        scrn_sfc.blit(bomb_sfc,bomb_rct)
+        # 練習６
+        bomb_rct.move_ip(vx, vy)
+        scrn_sfc.blit(bomb_sfc, bomb_rct) 
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+
+        if tori_rct.colliderect(bomb_rct):
+            return
+
         pg.display.update()
         clock.tick(1000)
 
---
+++ ex04/sample.py
@@ -0,0 +1,31 @@
+import pygame as pg
+import sys
+
+def main():
+    pg.display.set_caption("初めてのpygame")
+    pg.display.set_made((800,600))
+    tori_sfc=pg.image.load("fig/6.png")
+    tori_sfc=pg.transform.rotozoom(tori_sfc,0,2.0)
+    tori_rct=tori_sfc.get_rect()
+    tori_rct.center=400,300
+    scrn_sfc.blit(tori_sfc,tori_rct)
+
+    pg.display.update()
+
+
+
+    
+
+
+
+
+
+
+
+if __name__=="__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
+
+    
